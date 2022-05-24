20cacdf0382cfa332dc573501810ee7abef4132e
Vaibhav Solanki <vaibhavsolankie@gmail.com>
2022-05-24 16:03:16 +0530

    code splite

--- src/index.js
+++ src/index.js
@@ -4,66 +4,14 @@ const app = express();
 
 //cors policy
 var cors = require("cors");
-const { connect } = require("tls");
 app.use(cors());
 
+//chat server
+const chatServer = require("./configs/chatserver");
 const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-  },
-});
+chatServer(server);
 
 const PORT = process.env.PORT || 4000;
-const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
-
-io.on("connection", (socket) => {
-  console.log(`Client ${socket.id} connected`);
-
-  // Join a conversation
-  const { roomId } = socket.handshake.query;
-  socket.join(roomId);
-
-  //for socket length
-  // io.in(roomId)
-  //   .allSockets()
-  //   .then((res) => {
-  //     console.log(res.size);
-  //   });
-
-  // Listen for new messages
-  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
-    io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
-    newMes(data);
-  });
-  // socket.on("userCount", (data) => {
-  //   io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
-  //   newMes(data);
-  // });
-
-  // Leave the room if the user closes the socket
-  socket.on("disconnect", () => {
-    console.log(`Client ${socket.id} diconnected`);
-    socket.leave(roomId);
-  });
-});
-
-const model = require("./model/chat");
-const newMes = async (data) => {
-  try {
-    var room = await model.findOne({ room: data.roomId });
-    if (room) {
-      room.messages = [...room.messages, data];
-      await room.save();
-    } else
-      room = model.create({
-        room: data.roomId,
-        messages: [data],
-      });
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 app.get("/:id", async (req, res) => {
   const id = req.params.id;
